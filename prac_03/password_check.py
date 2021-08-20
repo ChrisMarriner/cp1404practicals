1d89a3f86eff8198482fecc5e7ac9fdeb5c05082
ChrisMarriner <christopher.marriner@my.jcu.edu.au>
2021-08-20 13:23:57 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -5,9 +5,21 @@ CP1404 - Password Checker
 
 MINIMUM_LENGTH = 4
 
-password = input("Password: ")
-while len(password) < MINIMUM_LENGTH:
-    password = input("Password needs to be at least {} characters: ".format(MINIMUM_LENGTH))
 
-print('*' * len(password))
+def main():
+    password = get_password()
+    print_asterisks(password)
 
+
+def print_asterisks(password):
+    print('*' * len(password))
+
+
+def get_password():
+    password = input("Password: ")
+    while len(password) < MINIMUM_LENGTH:
+        password = input("Password needs to be at least {} characters: ".format(MINIMUM_LENGTH))
+    return password
+
+
+main()
