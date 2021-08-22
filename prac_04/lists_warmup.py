89a4023201908de8dd8a0f54f06ebdc4008129cc
ChrisMarriner <christopher.marriner@my.jcu.edu.au>
2021-08-22 13:27:21 +1000

    Add List Warmup program to depo

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,14 @@
+"""
+Christopher Marriner - CP1404 - Lists Warm up
+"""
+
+numbers = [3, 1, 3, 1, 5, 9, 2]
+
+# 1.
+numbers[0] = "ten"
+# 2.
+numbers[-1] = "1"
+# 3.
+numbers[2:]
+# 4.
+"9" in numbers
