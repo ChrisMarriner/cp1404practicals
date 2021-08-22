6d755b2a43546414d2b027ffe402b9c3b5f29338
ChrisMarriner <christopher.marriner@my.jcu.edu.au>
2021-08-22 15:17:39 +1000

    Add user input and error checking

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,4 +1,29 @@
 """
 Christopher Marriner
 CP1404 - "Quick Pick" Lottery Ticket Generator
+
+Pseudocode:
+import random
+get number of quick picks
+error checking
+make empty list for quick picks
+
+display quick picks
+
 """
+
+import random
+
+
+def main():
+    number_of_quick_picks = int(input("Number of quick picks: "))
+    error_checking(number_of_quick_picks)
+
+
+def error_checking(number_of_quick_picks):
+    while number_of_quick_picks < 0:
+        print("Number of quick picks cannot be 0 or less.")
+        number_of_quick_picks = int(input("Number of quick picks: "))
+
+
+main()
