142c8bdbf06ccaa4917e002d31922744e0c00f46
ChrisMarriner <christopher.marriner@my.jcu.edu.au>
2021-08-22 16:25:14 +1000

    Fix error checking Fix in range statements Add print statement

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,31 +6,37 @@ Pseudocode:
 import random
 get number of quick picks
 error checking
-while in range of number of quick picks
+in range of number of quick picks
     make empty list for quick picks
+    in range of NUMBERS PER LINE
     generate random numbers
     add random numbers to quick picks list
+    sort quick picks list
 display quick picks
 
 """
 
 import random
 
+NUMBERS_PER_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
+
 
 def main():
     number_of_quick_picks = int(input("Number of quick picks: "))
-    error_checking(number_of_quick_picks)
-    for i in range(number_of_quick_picks):
-        quick_picks = []
-        number = random.randint(1, 46)
-    quick_picks.append(number)
-    print(quick_picks)
-
-
-def error_checking(number_of_quick_picks):
     while number_of_quick_picks < 0:
         print("Number of quick picks cannot be 0 or less.")
         number_of_quick_picks = int(input("Number of quick picks: "))
+    for i in range(number_of_quick_picks):
+        quick_picks = []
+        for x in range(NUMBERS_PER_LINE):
+            number = random.randint(MINIMUM, MAXIMUM)
+            while number in quick_picks:
+                number = random.randint(MINIMUM, MAXIMUM)
+            quick_picks.append(number)
+        quick_picks.sort()
+        print(" ".join("{:2}".format(number) for number in quick_picks))
 
 
 main()
