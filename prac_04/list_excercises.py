96633fc5a656f9f6a8d674962d8673f2fba97ab4
ChrisMarriner <christopher.marriner@my.jcu.edu.au>
2021-08-22 14:36:01 +1000

    Refactor programs with functions

--- prac_04/list_excercises.py
+++ prac_04/list_excercises.py
@@ -16,22 +16,26 @@ display the average of numbers
 
 """
 
-# NUMBER_OF_QUESTIONS = 5
-#
-#
-# def main():
-#     numbers = []
-#     for i in range(NUMBER_OF_QUESTIONS):
-#         number = int(input("Number: "))
-#         numbers.append(number)
-#     print("The first number is: ", numbers[0])
-#     print("the last number is: ", numbers[-1])
-#     print("The smallest number is: ", min(numbers))
-#     print("The largest number is: ", max(numbers))
-#     print("The average of the numbers is: ", sum(numbers) / len(numbers))
-#
-#
-# main()
+NUMBER_OF_QUESTIONS = 5
+
+
+def main():
+    numbers = []
+    for i in range(NUMBER_OF_QUESTIONS):
+        number = int(input("Number: "))
+        numbers.append(number)
+    display_numbers(numbers)
+
+
+def display_numbers(numbers):
+    print("The first number is: ", numbers[0])
+    print("the last number is: ", numbers[-1])
+    print("The smallest number is: ", min(numbers))
+    print("The largest number is: ", max(numbers))
+    print("The average of the numbers is: ", sum(numbers) / len(numbers))
+
+
+main()
 
 """
 # 2. Woefully inadequate security checker
@@ -51,6 +55,10 @@ def main():
                  'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
                  'bob']
     username = input("Username: ")
+    display_access_permission(username, usernames)
+
+
+def display_access_permission(username, usernames):
     if username in usernames:
         print("Access Granted!")
     else:
