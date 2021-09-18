118cac40e4cee06808dad24e5675ab54984ef0f7
ChrisMarriner <christopher.marriner@my.jcu.edu.au>
2021-09-18 16:42:18 +1000

    define __init__ in Guitar class

---
+++ prac_06/guitar.py
@@ -0,0 +1,11 @@
+"""
+Guitar class
+Christopher Marriner
+"""
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
