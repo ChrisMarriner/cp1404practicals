aed3bdd2c6a68fca9f0775a9c4a441c5c1c705e9
ChrisMarriner <christopher.marriner@my.jcu.edu.au>
2021-09-18 15:50:03 +1000

    Add __init__ to ProgrammingLanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,12 @@
+"""
+Christopher Marriner
+Programming Language program
+"""
+
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year - year
