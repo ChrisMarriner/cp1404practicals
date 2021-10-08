749278cc1baaa7595d60cf019463eed7727ac88c
ChrisMarriner <christopher.marriner@my.jcu.edu.au>
2021-10-09 08:53:34 +1000

    Add main function to unreliable car test program

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -2,3 +2,20 @@
 Christopher Marriner
 unreliable car test program
 """
+
+from prac_08.unreliable_car import UnreliableCar
+
+
+def main():
+    """Testing different cars on reliability multiple times"""
+    car_1 = UnreliableCar("Good", 100, 90)
+    car_2 = UnreliableCar("Bad", 100, 9)
+    for i in range(1, 12):
+        print("Attempting to drive {}km:".format(i))
+        print("{:12} drove {:2}km".format(car_1.name, car_1.drive(i)))
+        print("{:12} drove {:2}km".format(car_2.name, car_2.drive(i)))
+    print(car_1)
+    print(car_2)
+
+
+main()
