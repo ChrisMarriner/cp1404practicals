ff11217e0dc234c326a21bc6b334be11353cc536
ChrisMarriner <christopher.marriner@my.jcu.edu.au>
2021-10-10 11:07:11 +1000

    Add test program for silver service taxi

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -2,3 +2,29 @@
 Christopher Marriner
 Silver Service Taxi program
 """
+
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+
+from prac_08.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of a Car that includes fare costs."""
+    flagfall = 4.5
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a Taxi instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        """Return a string like a Car but with current fare distance."""
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(), self.flagfall)
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return self.flagfall + super().get_fare()
